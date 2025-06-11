87b395ce82c420895bd99188475feac6e125f267
1speedyako <charloako1@gmail.com>
2025-06-11 18:46:32 +0000

    services updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Home from "./components/home";
 import ContactPage from "./pages/contact";
 import AboutPage from "./pages/about";
 import ServicesPage from "./pages/services";
+import SEOPage from "./pages/services/seo";
+import GoogleAdsPage from "./pages/services/google-ads";
+import BrandMarketingPage from "./pages/services/brand-marketing";
+import CybersecurityPage from "./pages/services/cybersecurity";
 import routes from "tempo-routes";
 
 function App() {
@@ -15,6 +19,16 @@ function App() {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<ServicesPage />} />
+          <Route path="/services/seo" element={<SEOPage />} />
+          <Route path="/services/google-ads" element={<GoogleAdsPage />} />
+          <Route
+            path="/services/brand-marketing"
+            element={<BrandMarketingPage />}
+          />
+          <Route
+            path="/services/cybersecurity"
+            element={<CybersecurityPage />}
+          />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
